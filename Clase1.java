306cc5b33eaf2289305e1430e5a491fc387b78a8
fernandolopezjava <67985041+fernandolopezjava@users.noreply.github.com>
2020-08-25 23:34:51 -0300

    primeros comandos declaracion de variables
    primeros comandos declaracion de variables, salida y entrada de datos

---
+++ Clase1.java
@@ -0,0 +1,55 @@
+
+package clase.pkg1;
+
+
+public class Clase1 {
+
+    
+    public static void main(String[] args) {
+        
+       System.out.println("hola mundo");
+       short num;
+       
+       num=26;
+       
+       System.out.println(num);
+       
+       num=350;
+       
+       System.out.println(num);
+       
+       float numReal;
+       
+       numReal=15.27534782418618181253f;
+       
+       System.out.println(numReal); 
+       
+       double numReal2;
+       
+       numReal2=15.27534782418618181253;
+       
+       System.out.println(numReal2); 
+       
+       char letra = 'g';
+       
+       System.out.println(letra); 
+       
+       letra = 64;
+       
+       System.out.println(letra); 
+       
+       String txt="hola";
+       
+       System.out.println(txt);
+       
+       boolean log = true;
+       
+       System.out.println(log);
+     
+       
+
+       
+       
+    }
+    
+}
